52d3a0daf515e421723c759d73d593f608bd71d4
carello <cp2carello@gmail.com>
2019-09-05 23:09:20 -0500

    added template directory

---
+++ creating-mocks/create_testbed_mocks-1.2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import csv
+from jinja2 import Template
+from jinja2 import Environment
+from jinja2 import FileSystemLoader
+
+j2_env = Environment(loader=FileSystemLoader('templates'))
+
+name = "cp-mock-normal"
+mock_file = "outputs/mock-break-zz.yaml"
+
+tmpl_0 = j2_env.get_template('tmpl_0.j2')
+tmpl_A = j2_env.get_template('tmpl_A.j2')
+
+header = (tmpl_0.render(
+    NAME=name
+))
+
+with open(mock_file, 'w') as fh:
+    fh.write(header)
+
+data = list()
+# CSV format is MS-DOS
+with open("data3.1.csv", newline='') as f:
+    reader = csv.DictReader(f)
+    for r in reader:
+        info = (r['DEVICE'], r['ALIAS'], r['OS'], r['TYPE'], r['CLASS'], r['COMMAND'], r['PROTOCOL'], r['ORDER'])
+        content = (tmpl_A.render(
+            DEVICE=info[0],
+            ALIAS=info[1],
+            OS=info[2],
+            TYPE=info[3],
+            CLASS=info[4],
+            COMMAND=info[5],
+            PROTOCOL=info[6],
+            ORDER=info[7]
+        ))
+
+        with open(mock_file, 'a') as fw:
+            fw.write(content)
